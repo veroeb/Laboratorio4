0ed2eaabc667a1afbdd3e0e9ac0542e081fdff50
Veronica <vecheza@gmail.com>
2018-05-28 19:43:24 -0300

    prueba con DateTime en Program.cs

---
+++ Laboratorio 4 - Excepciones/Laboratorio 4 - Excepciones/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_4___Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string d = DateTime.Now.ToString();
+            Console.WriteLine(d);
+            Console.ReadLine();
+        }
+    }
+}
